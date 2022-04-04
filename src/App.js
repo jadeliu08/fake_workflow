6141e5f19519ac7fae1fb34054cefda808a8350c
lcq <1005428477@qq.com>
2022-04-04 22:20:46 +0800

    example: Basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import {Link} from "react-router-dom";
+import React from "react";
+import {Switch, Route, Link, useParams, useRouteMatch} from "react-router-dom";
 
 function App() {
     return (
         <div>
-            <h1>Welcome to learn React Router lessons!</h1>
-            <Link to="/invoices">Invoices</Link>|
-            <Link to="/expenses">Expenses</Link>
+            <div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/topics">Topics</Link></li>
+                </ul>
+                <Switch>
+                    <Route path="/about" component={About}/>
+                    <Route path="/topics" component={Topics}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
+            </div>
         </div>
     );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Topics() {
+    let match = useRouteMatch();
+    return (
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                <li><Link to={`${match.url}/components`}>Components</Link></li>
+                <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
+            </ul>
+            <Switch>
+                <Route path={`${match.path}/:topicId`} component={Topic}/>
+                <Route path={match.path}><h3>Please select a topic.</h3></Route>
+            </Switch>
+        </div>
+    );
+}
+
+function Topic() {
+    let {topicId} = useParams();
+    return <h3>Requested topic ID: {topicId}</h3>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 import './index.css';
 import App from './App';
-import Expenses from "./expense";
-import Invoices, {Invoice} from "./invoice";
 
 
 ReactDOM.render(
     <BrowserRouter>
-        <Route path="/" component={App} />
-        <Route path="/expenses" component={Expenses}/>
-        <Route path="/invoices" component={Invoices} />
-        <Route path="/invoices:id" component={Invoice}/>
-    </BrowserRouter>,
+        <App/>
+    </BrowserRouter>
+    ,
     document.getElementById('root')
 );
 
