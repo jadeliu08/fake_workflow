cdff6bd8123ec59f3b3cab4137bf78ed63d80463
lcq <1005428477@qq.com>
2022-04-09 14:32:55 +0800

    add axios package

--- src/header/index.js
+++ src/header/index.js
@@ -8,7 +8,7 @@ function Header() {
             <img src={process.env.PUBLIC_URL + "OceanXWorkflow.png"} alt="OceanXWorkflow" className="header-logo"/>
             <ul className="header-menu nav">
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/inbox">Inbox</Link></li>
+                <li><Link to="/tasks">Inbox</Link></li>
                 <li><Link to="/workflow/admin">Workflow Admin</Link></li>
                 <li><Link to="/language">Language</Link></li>
                 <li><Link to="/version">Version</Link></li>
