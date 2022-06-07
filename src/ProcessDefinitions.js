724689470962d0c7e133015df9073f359ff2a4b4
lcq <1005428477@qq.com>
2022-06-07 20:03:40 +0800

    update

--- src/ProcessDefinitions.js
+++ src/ProcessDefinitions.js
@@ -9,7 +9,7 @@ function ProcessDefinitionList() {
     const dialogProps = {
         title: "发起流程", autoFocus: true, width: "500px", height: "300px",
         onClose: () => {
-            setDialogOptions({visible:false, definition: null});
+            setDialogOptions({visible: false, definition: null});
         }
     }
 
@@ -17,26 +17,29 @@ function ProcessDefinitionList() {
         setDialogOptions({visible: true, definition: dataItem});
     }
 
-    return <div className="k-d-no-gap-flex">
-        {processDefinitionDataSource.map(item => {
-            return <Card key={item.id} className="k-cursor-pointer" style={{fontSize: "12px"}} onClick={function () {
-                handleClick(item);
-            }}>
-                <CardBody>
-                    <CardTitle></CardTitle>
-                    <div className="k-pb-1"><span>版本：</span>{item.version}</div>
-                    <div className="k-pb-1"><span>流程名称：</span>{item.name}</div>
-                    <div><span>签章流程：</span>{item.signature ? "是" : "否"}</div>
-                </CardBody>
-            </Card>;
-        })}
-        {
-            dialogOptions.visible && <Dialog {...dialogProps}>
-                <div><span>版本: </span>{dialogOptions.definition && dialogOptions.definition.version}</div>
-                <div><span>版本: </span>{dialogOptions.definition && dialogOptions.definition.name}</div>
-            </Dialog>
-        }
-    </div>;
+    return <>
+        <div className="k-d-no-gap-flex">
+            {processDefinitionDataSource.map(item => {
+                return <Card key={item.id} className="k-cursor-pointer" style={{fontSize: "12px"}} onClick={function () {
+                    handleClick(item);
+                }}>
+                    <CardBody>
+                        <CardTitle></CardTitle>
+                        <div className="k-pb-1"><span>版本：</span>{item.version}</div>
+                        <div className="k-pb-1"><span>流程名称：</span>{item.name}</div>
+                        <div><span>签章流程：</span>{item.signature ? "是" : "否"}</div>
+                    </CardBody>
+                </Card>;
+            })}
+            {
+                dialogOptions.visible && <Dialog {...dialogProps}>
+                    <div><span>版本: </span>{dialogOptions.definition && dialogOptions.definition.version}</div>
+                    <div><span>流程: </span>{dialogOptions.definition && dialogOptions.definition.name}</div>
+                </Dialog>
+            }
+        </div>
+    </>
+        ;
 }
 
 export {ProcessDefinitionList};
