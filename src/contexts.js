1e0d52325bd57d994b0db6f01645f40f1b998d44
lcq <1005428477@qq.com>
2022-06-11 11:20:41 +0800

    update

--- src/contexts.js
+++ src/contexts.js
@@ -1,3 +1,4 @@
 import {createContext} from "react";
 const authContext = createContext({logged: localStorage.getItem("logged") === "true"});
+authContext.displayName = "AuthContext";
 export {authContext};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import {Router, } from "react-router-dom";
+import {createBrowserHistory} from 'history';
 
 
 import './index.css';
 import App from "./App";
 
+//第一种写法
+const history = createBrowserHistory();
 ReactDOM.render(
-    <BrowserRouter>
+    <Router history={history}>
         <App/>
-    </BrowserRouter>,
+    </Router>,
     document.getElementById('root')
 );
+//第二种写法
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <App/>
+//     </BrowserRouter>,
+//     document.getElementById('root')
+// );
