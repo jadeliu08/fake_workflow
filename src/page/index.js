c4d5388d8bcc5775de0fba5435b29ecedcdf2892
jadeliu08 <changqingliu08@163.com>
2025-09-22 17:30:47 +0800

    update

--- src/page/index.js
+++ src/page/index.js
@@ -1,49 +1,49 @@
-import React, {useContext} from "react";
-import {Redirect, Switch, Route} from "react-router-dom";
-import {Provider} from "react-redux";
-import store from "../reduxes/store";
+import React, { useContext } from 'react';
+import { Redirect, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../reduxes/store';
 
-import {authContext} from "../contexts";
-import OceanXAuth from "../authentication";
-import Header from "../header";
-import "./index.css";
-import UserSidebar from "../userSidebar";
-import Tasks from "../Tasks";
-import {ProcessDefinitionList} from "../ProcessDefinitions";
-import {ProcessInstanceList} from "../ProcessInstances";
-import HistoryTaskGrid from "../tasks/historytask";
+import { authContext } from '../contexts';
+import OceanXAuth from '../authentication';
+import Header from '../header';
+import './index.css';
+import UserSidebar from '../userSidebar';
+import Tasks from '../Tasks';
+import { ProcessDefinitionList } from '../ProcessDefinitions';
+import { ProcessInstanceList } from '../ProcessInstances';
+import HistoryTaskGrid from '../tasks/historytask';
 
 
 function Container() {
-    return (
-        <Switch>
-            <Route exact path="/tasks" component={Tasks}/>
-            <Route path="/tasks/history" component={HistoryTaskGrid}/>
-            <Route path="/process_definitions" component={ProcessDefinitionList}/>
-            <Route path="/process_instances" component={ProcessInstanceList}/>
-        </Switch>
-    );
+	return (
+			<Switch>
+				<Route exact path="/tasks" component={Tasks}/>
+				<Route path="/tasks/history" component={HistoryTaskGrid}/>
+				<Route path="/process_definitions" component={ProcessDefinitionList}/>
+				<Route path="/process_instances" component={ProcessInstanceList}/>
+			</Switch>
+	);
 }
 
 function Page() {
-    const authContextValue = useContext(authContext);
-    return authContextValue.logged ?
-        <OceanXAuth>
-            <Provider store={store}>
-                <div className="page">
-                    <div className="header">
-                        <Header/>
-                    </div>
-                    <div className="sidebar">
-                        <UserSidebar/>
-                    </div>
-                    <div className="content">
-                        <Container/>
-                    </div>
-                </div>
-            </Provider>
-        </OceanXAuth>
-        : <Redirect to="/login"/>;
+	const authContextValue = useContext(authContext);
+	return authContextValue.logged ?
+			<OceanXAuth>
+				<Provider store={store}>
+					<div className="page">
+						<div className="header">
+							<Header/>
+						</div>
+						<div className="sidebar">
+							<UserSidebar/>
+						</div>
+						<div className="content">
+							<Container/>
+						</div>
+					</div>
+				</Provider>
+			</OceanXAuth>
+			: <Redirect to="/login"/>;
 }
 
 export default Page;
